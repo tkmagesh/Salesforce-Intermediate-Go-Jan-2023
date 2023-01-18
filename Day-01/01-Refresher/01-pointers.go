5f8233de78e297825272e8b0b2540a170fb18fec
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-18 15:54:41 +0530

    pointers on arrays and slices

--- Day-01/01-Refresher/01-pointers.go
+++ Day-01/01-Refresher/01-pointers.go
@@ -16,28 +16,37 @@ type Employee struct {
 	Salary float32
 }
 
-func (emp Employee) Format() string {
-	return fmt.Sprintf("Id = %d, Name = %q, Salary = %.2f", emp.Id, emp.Name, emp.Salary)
+func (this Employee) WhoAmI() {
+	fmt.Println("I am an employee - ", this.Name)
 }
 
-func (emp *Employee) AwardBonus(bonus float32) {
-	emp.Salary += bonus
+func (this Employee) Format() string {
+	return fmt.Sprintf("Id = %d, Name = %q, Salary = %.2f", this.Id, this.Name, this.Salary)
+}
+
+func (this *Employee) AwardBonus(bonus float32) {
+	this.Salary += bonus
 }
 
 func main() {
+
+	/* Arrays are values */
 	nos1 := [5]int{3, 1, 4, 2, 5}
 	nos2 := [5]int{3, 1, 4, 2, 5}
 	fmt.Println(nos1 == nos2)
 
+	/* Structs are also values */
 	p1 := Product{100, "Pen", 10}
 	p2 := Product{100, "Pen", 10}
 	fmt.Println(p1 == p2)
 
+	/* Use reference (aka pointers) to change the state */
 	pen := Product{100, "Pen", 10}
 	fmt.Println("Before Applying Discount, pen -", pen)
 	ApplyDiscount(&pen, 10)
 	fmt.Println("After Applying Discount, pen -", pen)
 
+	//Pointer syntax overview
 	var no int
 	no = 100
 
@@ -52,14 +61,17 @@ func main() {
 
 	fmt.Println(no == *(&no))
 
+	//Using pointers
 	fmt.Println("Before incrementing, no =", no)
 	increment(&no)
 	fmt.Println("After incrementing, no =", no)
 
+	//Accessing fields of struct pointers
 	pencil := &Product{200, "Pencil", 10}
 	fmt.Println((*pencil).Id)
 	fmt.Println(pencil.Id)
 
+	//Pointers in method receivers
 	emp := Employee{
 		Id:     100,
 		Name:   "Magesh",
@@ -71,6 +83,43 @@ func main() {
 	// (&emp).AwardBonus(2000) // DONOT have to do this
 	emp.AwardBonus(2000)
 	fmt.Println(emp.Format())
+
+	// passing arrays as arguments
+	fmt.Println("Arrays")
+	nosArray := [5]int{3, 1, 4, 2, 5}
+	fmt.Println(nosArray)
+	sortArray(&nosArray)
+	fmt.Println(nosArray)
+
+	//slice
+	fmt.Println("Slices")
+	nosSlice := []int{3, 1, 4, 2, 5}
+	fmt.Println(nosSlice)
+	sortSlice(nosSlice)
+	fmt.Println(nosSlice)
+
+}
+
+func sortArray(list *[5]int) {
+	for i := 0; i < (len(list) - 1); i++ {
+		for j := i + 1; j < len(list); j++ {
+			if list[i] > list[j] {
+				list[i], list[j] = list[j], list[i]
+			}
+		}
+	}
+	fmt.Println("[sortArray], list = ", list)
+}
+
+func sortSlice(list []int) {
+	for i := 0; i < (len(list) - 1); i++ {
+		for j := i + 1; j < len(list); j++ {
+			if list[i] > list[j] {
+				list[i], list[j] = list[j], list[i]
+			}
+		}
+	}
+	fmt.Println("[sortSlice], list = ", list)
 }
 
 func increment(val *int) {
